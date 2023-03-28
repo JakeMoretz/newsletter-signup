a31187909a8df7ff2809c4a46d6217db7783e732
Jake Moretz <jmoretz9090@gmail.com>
2023-03-28 15:04:16 -0400

    working on linking up the api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const request = require("request");
+const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,17 +16,50 @@ app.get('/', function (req, res) {
 
 // server grabbing the info from the home route html file
 app.post('/', function (req, res) {
-    let fName = req.body.firstName;
-    let lNAme = req.body.lastName;
-    let email = req.body.email;
+    const fName = req.body.firstName;
+    const lName = req.body.lastName;
+    const email = req.body.email;
+
+    // data object, mailchimp tells us how they want his layed out
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_field: {
+                    FNAME: fName,
+                    LNAME: lName,
+                },
+            },
+        ],
+    };
+
+    // change object into string
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://<dc>.api.mailchimp.com/3.0/lists/4f87f68cb6';
+
+    const options = {
+        method: 'POST',
+        auth: "5ca65a5b71161d517a242b6c173caacd-us17"
+    };
+
+    const request = https(url, options, function (response) {
+        response.on('data', function (data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
 });
 
 app.listen(3000, function () {
     console.log('server is running on port 3000');
 });
 
-// api key
-// 1968eac2a3b54937dbdab59480b05018-us17
+// https://us17.admin.mailchimp.com/
 
-// Id
+// 5ca65a5b71161d517a242b6c173caacd-us17
+
 // 4f87f68cb6
